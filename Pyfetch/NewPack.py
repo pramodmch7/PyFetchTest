68b80318109cc37cf0e6eb7f4bce257c85bed0bb
pramodmch7 <pramodmc.h7@gmail.com>
2020-06-27 11:27:40 +0530

    Update NewPack.py

--- Pyfetch/NewPack.py
+++ Pyfetch/NewPack.py
@@ -2,3 +2,4 @@ def Tr(Self):
   print(Self)
   print('HanUmaN form New 79 DVP 79!!!!!!!!97!!!!!!!777777799999997777777')
   print('Application Started Running')
+  print('Application Started Running in port 7000')
